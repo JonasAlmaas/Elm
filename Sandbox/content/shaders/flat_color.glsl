7eb9ffd9c6dbd6505de1d02f27f01ab16425b247
Jonas Almaas <jonas.f.almaas@gmail.com>
2025-03-04 16:47:24 +0100

    Cleanup flat color shader

--- Sandbox/content/shaders/flat_color.glsl
+++ Sandbox/content/shaders/flat_color.glsl
@@ -6,8 +6,6 @@ layout(location = 0) in vec3 a_position;
 uniform mat4 u_view_projection;
 uniform mat4 u_transform;
 
-out vec4 v_color;
-
 void main()
 {
 	gl_Position = u_view_projection * u_transform * vec4(a_position, 1.0);
