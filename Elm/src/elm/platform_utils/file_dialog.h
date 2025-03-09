b73b1249262c6c74fea5a53f3140f3cd7d8f5349
Jonas Almaas <jonas.f.almaas@gmail.com>
2025-03-09 16:56:07 +0100

    Add file dialog filter example

--- Elm/src/elm/platform_utils/file_dialog.h
+++ Elm/src/elm/platform_utils/file_dialog.h
@@ -6,11 +6,13 @@
 namespace elm::file_dialog {
 
 	/**
+	 * @param filter E.g. "Model file (*.fbx)\0*.fbx\0"
 	 * @return path to file. Empty string if canceled.
 	 */
 	extern std::string open_file(const char *filter);
 
 	/**
+	 * @param filter E.g. "Model file (*.fbx)\0*.fbx\0"
 	 * @return path to directory. Empty string if canceled.
 	 */
 	extern std::string save_file(const char *filter);
