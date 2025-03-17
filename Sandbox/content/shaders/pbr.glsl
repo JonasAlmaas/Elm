2dca6d0d574609b49bb8c78e154a4c583ae5ee5b
Jonas Almaas <jonas.f.almaas@gmail.com>
2025-03-17 13:15:39 +0100

    Remove texture slot from pbr shader

--- Sandbox/content/shaders/pbr.glsl
+++ Sandbox/content/shaders/pbr.glsl
@@ -4,7 +4,6 @@
 layout (location = 0) in vec3 a_position;
 layout (location = 1) in vec2 a_uv;
 layout (location = 2) in vec3 a_normal;
-layout (location = 3) in int a_texture_slot; // TODO: Remove
 
 layout (std140, binding = 0) uniform camera
 {
